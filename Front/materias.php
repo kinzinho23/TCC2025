d6871b6635d20d228e7c195bd6052767a8c09f15
Papai Galo <dacunhasanchezz@gmail.com>
2026-02-24 22:34:14 -0300

    Começa tela de materias

---
+++ Front/materias.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="materias.css">
+    <title>Matérias</title>
+</head>
+<body>
+     <header>
+        <?php include 'sidebar.php'; ?>
+    </header>
+    <main>
+       <h1 id="title">Matérias</h1>
+
+       <div class="materia-item">
+         <div class="materia-name">Matemática <?php //echo $materia_id; ?>
+         <div class="materia-info">(conteúdo atual da matería)</div>
+
+         </div>
+    </main>
+</body>
+</html>
